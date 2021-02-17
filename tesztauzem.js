9c302c5ddac31db9331e592effdd3db663c71661
daniellukacsi <70635327+daniellukacsi@users.noreply.github.com>
2021-02-17 11:20:14 +0100

    tesztauzem feladat

---
+++ tesztauzem.js
@@ -0,0 +1,12 @@
+﻿window.addEventListener('load',function(){
+
+})
+
+function kuldes(){
+    if(document.getElementById("jatek-valasz").value==""){
+        alert("A mező üres!")
+    }
+    else{document.getElementById("jatek-valasz").value=="";
+        alert("Köszönjük, hogy részt vesz a játékunkban!");
+}
+}
