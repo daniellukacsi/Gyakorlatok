4ceca67540cc7c111bebc209be733a8d6f916c73
daniellukacsi <70635327+daniellukacsi@users.noreply.github.com>
2021-03-08 12:28:49 +0100

    oldatok html bootstrapes

---
+++ bootstrapes/oldat.js
@@ -0,0 +1,8 @@
+function szamol(){
+    var moa=document.getElementById("so").value;
+    var mosz=document.getElementById("viz").value;
+    var so=parseFloat(moa);
+    var viz=parseFloat(mosz);            
+    var mm=(so/(so+viz))*100;
+    document.getElementById("eredmeny").innerHTML=mm;
+}
