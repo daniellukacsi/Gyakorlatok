0570bd92909523f418fb8562ab930e55fa3785dc
daniellukacsi <70635327+daniellukacsi@users.noreply.github.com>
2021-03-22 10:46:16 +0100

    filmnoir kép klikk esemény js

---
+++ filmnoir/filmnoir.js
@@ -0,0 +1,12 @@
+window.addEventListener('load',function(){
+})
+
+function katt(){
+    if(img){
+        document.getElementById("img").style.display="none"
+        return img=0;
+    }else{
+        document.getElementById("img").style.display="inline" 
+        return img=1;
+    }
+}
